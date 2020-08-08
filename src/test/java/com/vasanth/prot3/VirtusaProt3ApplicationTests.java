4cc8606f7551eba1ec76a6ad25beaa4425bcfbaa
vasanthageethan <44281300+vasanthageethan@users.noreply.github.com>
2020-08-08 10:12:06 +0530

    Add files via upload

---
+++ src/test/java/com/vasanth/prot3/VirtusaProt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasanth.prot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtusaProt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
